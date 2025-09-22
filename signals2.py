29acabadcc467b15599be79b0f36603ef1e501df
Casper Lugtmeijer <clugtmeijer@tudelft.nl>
2025-09-22 17:21:11 +0200

    Fixing np.py to np.pi

--- signals2.py
+++ signals2.py
@@ -2,5 +2,5 @@ import numpy as np
 def generate_sine_wave(frequency, duration, sample_rate):
     N = int(duration * sample_rate) # Shows total number of samples
     t = np.arange(N) / sample_rate # Is the time vector
-    x = np.sin(2 * np.py * frequency * t) # Builds the real sine-wave samples
+    x = np.sin(2 * np.pi * frequency * t) # Builds the real sine-wave samples
     return t, x

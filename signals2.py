5f40c813077b630895ebe85efc3113e46e8d5acf
Casper Lugtmeijer <clugtmeijer@tudelft.nl>
2025-09-22 16:57:20 +0200

    Add new file signals2.py

---
+++ signals2.py
@@ -0,0 +1,6 @@
+import numpy as np
+def generate_sine_wave(frequency, duration, sample_rate):
+    N = int(duration * sample_rate) # Shows total number of samples
+    t = np.arange(N) / sample_rate # Is the time vector
+    x = np.sin(2 * np.py * frequency * t) # Builds the real sine-wave samples
+    return t, x

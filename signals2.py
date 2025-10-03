43daeaad8dd83b72ea31f97a69b8fbdf826c533b
CasperLugtmeijer <clugtmeijer@tudelft.nl>
2025-10-03 13:28:37 +0200

    Creating a square function

--- signals2.py
+++ signals2.py
@@ -4,3 +4,10 @@ def generate_sine_wave(frequency, duration, sample_rate):
     t = np.arange(N) / sample_rate # Is the time vector
     x = np.sin(2 * np.pi * frequency * t) # Builds the real sine-wave samples
     return t, x
+
+def genarate_square(frequency, duration, sample_rate, amplitude = 1.0, duty = 0.5):
+    N = int(duration * sample_rate)
+    t = np.arange(N) / sample_rate
+    phase = (t * frequency) % 1.0 # phase within each period (0,1)
+    x = np.where(phase < duty, amplitude, -amplitude) #
+    return t, x

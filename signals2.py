603d34d5704c7c5377f247b5e237128a0a368648
CasperLugtmeijer <clugtmeijer@tudelft.nl>
2025-10-03 14:41:04 +0200

    adding time shift function

--- signals2.py
+++ signals2.py
@@ -9,5 +9,15 @@ def generate_square(frequency, duration, sample_rate, amplitude = 1.0, duty = 0.
     N = int(duration * sample_rate)
     t = np.arange(N) / sample_rate
     phase = (t * frequency) % 1.0 # phase within each period (0,1)
-    x = np.where(phase < duty, amplitude, -amplitude) #
+    x = np.where(phase < duty, amplitude, -amplitude) 
     return t, x
+
+def time_shift(t, x, shift_seconds, sample_rate):
+    n = int(round(shift_seconds * sample_rate))
+    y = np.zeros_like(x)
+    if n >= 0:
+        y[n:]=x[:len(x)-n]
+    else:
+        y[len(x)=n] = x[-n:]
+
+return t, y

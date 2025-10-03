5b6604132a3ada579ea097ce12a92fc2afd70167
CasperLugtmeijer <clugtmeijer@tudelft.nl>
2025-10-03 13:45:31 +0200

    From genarate to generate

--- signals2.py
+++ signals2.py
@@ -5,7 +5,7 @@ def generate_sine_wave(frequency, duration, sample_rate):
     x = np.sin(2 * np.pi * frequency * t) # Builds the real sine-wave samples
     return t, x
 
-def genarate_square(frequency, duration, sample_rate, amplitude = 1.0, duty = 0.5):
+def generate_square(frequency, duration, sample_rate, amplitude = 1.0, duty = 0.5):
     N = int(duration * sample_rate)
     t = np.arange(N) / sample_rate
     phase = (t * frequency) % 1.0 # phase within each period (0,1)

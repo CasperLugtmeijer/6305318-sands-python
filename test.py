2d6bc037c87b458b12e4136a02d82c887152f347
CasperLugtmeijer <clugtmeijer@tudelft.nl>
2025-10-06 16:41:47 +0200

    Multline docstring

--- test.py
+++ test.py
@@ -3,3 +3,17 @@ def add (a,b):
     return a + b
 
 help(add)
+
+def multiply (a,b):
+    """
+    Mutliply two numbers
+
+    Parameters:
+    a (int/float): First number
+    b (int/float): Second number
+
+    Returns:
+    int/float: Product of a and b
+    """
+    return a * b
+    

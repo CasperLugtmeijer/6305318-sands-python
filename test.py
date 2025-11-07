a7a64c32a484b2ef8fadb313f5681541f33508ed
CasperLugtmeijer <clugtmeijer@tudelft.nl>
2025-11-07 14:32:39 +0100

    Creating a test for sine

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
-def add (a, b):
-    """return the sum of a and b"""
-    return a + b
+import numpy as np
+from signals2 import generate_sine_wave, generate_square, time_shift, add_signals
 
-help(add)
+def test_generate_sine_wave():
+    t, y = generate_sine_wave(5, 2, 100)
+    assert len(t) == 200
+    assert y[0] == 0
 
-def multiply (a, b):
-    """
-    Mutliply two numbers
+    fs, f = 100, 5
+    k = int(fs / (4 * f))
+    assert np.isclose(y[k], 1.0, atol=1e-6)
 
-    Parameters:
-    a (int/float): First number
-    b (int/float): Second number
-
-    Returns:
-    int/float: Product of a and b
-    """
-    return a * b
-
-help(multiply)
+    t0, y0 = generate_sine_wave(5, 0, 100)
+    assert len(t0) == 0 and len(y0) == 0
